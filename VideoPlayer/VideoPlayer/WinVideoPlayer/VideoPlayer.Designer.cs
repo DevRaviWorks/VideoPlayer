6426b88eea27c014b6049b511d3ab8481d23b922
RAVI-PC\Ravi <pundir.ravi.yoyo@gmail.com>
2022-07-24 15:40:39 +0530

    Initial commit for video player

---
+++ VideoPlayer/VideoPlayer/WinVideoPlayer/VideoPlayer.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace WinVideoPlayer
+{
+    partial class VideoPlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VideoPlayer));
+            this.mediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.btn_fullScreen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mediaPlayer
+            // 
+            this.mediaPlayer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mediaPlayer.Enabled = true;
+            this.mediaPlayer.Location = new System.Drawing.Point(13, 0);
+            this.mediaPlayer.Margin = new System.Windows.Forms.Padding(3, 3, 3, 20);
+            this.mediaPlayer.Name = "mediaPlayer";
+            this.mediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mediaPlayer.OcxState")));
+            this.mediaPlayer.Size = new System.Drawing.Size(571, 314);
+            this.mediaPlayer.TabIndex = 0;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Start.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_Start.Location = new System.Drawing.Point(13, 397);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(111, 41);
+            this.btn_Start.TabIndex = 1;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = false;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Stop.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_Stop.Location = new System.Drawing.Point(302, 397);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(111, 41);
+            this.btn_Stop.TabIndex = 2;
+            this.btn_Stop.Text = "Stop";
+            this.btn_Stop.UseVisualStyleBackColor = false;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // btn_fullScreen
+            // 
+            this.btn_fullScreen.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_fullScreen.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_fullScreen.Location = new System.Drawing.Point(627, 397);
+            this.btn_fullScreen.Name = "btn_fullScreen";
+            this.btn_fullScreen.Size = new System.Drawing.Size(111, 41);
+            this.btn_fullScreen.TabIndex = 3;
+            this.btn_fullScreen.Text = "Full Screen";
+            this.btn_fullScreen.UseVisualStyleBackColor = false;
+            this.btn_fullScreen.Click += new System.EventHandler(this.btn_fullScreen_Click);
+            // 
+            // VideoPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_fullScreen);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.mediaPlayer);
+            this.Name = "VideoPlayer";
+            this.Text = "VideoPlayer";
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.Windows.Forms.Button btn_fullScreen;
+        protected AxWMPLib.AxWindowsMediaPlayer mediaPlayer;
+    }
+}
---
+++ VideoPlayer/VideoPlayer/WinVideoPlayer/VideoPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinVideoPlayer
+{
+    public partial class VideoPlayer : Form
+    {
+        public VideoPlayer()
+        {
+            InitializeComponent();         
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            mediaPlayer.URL = AppDomain.CurrentDomain.BaseDirectory + "\\Video\\sample-Video.mp4";
+            mediaPlayer.Ctlcontrols.play();
+        }
+
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            mediaPlayer.Ctlcontrols.stop();
+        }
+
+        private void btn_fullScreen_Click(object sender, EventArgs e)
+        {
+            mediaPlayer.fullScreen = mediaPlayer.URL.Length > 1;
+        }
+    }
+}
